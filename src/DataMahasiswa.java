f5dbb016bd755445f203c6343cc0f51ca939abb3
Hesaa23 <mahesamn23@gmail.com>
2024-11-09 22:56:31 +0700

    first commit

---
+++ src/DataMahasiswa.java
@@ -0,0 +1,31 @@
+public class DataMahasiswa {
+    private String NIM;
+    private String nama;
+    private String alamat;
+    private String matkul;
+    private double nilaiAkhir;
+    
+    public DataMahasiswa(String NIM, String nama, String alamat, String matkul, double nilaiAkhir){
+        this.NIM = NIM;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.matkul = matkul;
+        this.nilaiAkhir = nilaiAkhir;
+    }
+    
+    public String getNIM(){
+        return NIM;
+    }
+    public String getNama(){
+        return nama;
+    }
+    public String getAlamat(){
+        return alamat;
+    }
+    public String getMatkul(){
+        return matkul;
+    }
+    public double getNilaiAkhir(){
+        return nilaiAkhir;
+    }
+}
